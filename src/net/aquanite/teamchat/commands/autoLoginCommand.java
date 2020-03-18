6b93f7415e5af6d71fabb39ba1402a4d67cf9f86
Chaosmatter <Chaosmatter@gmx.de>
2020-03-18 08:56:30 +0100

    Update

---
+++ src/net/aquanite/teamchat/commands/autoLoginCommand.java
@@ -0,0 +1,4 @@
+package net.aquanite.teamchat.commands;
+
+public class autoLoginCommand  {
+}
--- src/net/aquanite/teamchat/commands/tcCommand.java
+++ src/net/aquanite/teamchat/commands/tcCommand.java
@@ -116,15 +116,16 @@ public class tcCommand extends Command {
                         }
                     } else if (args[0].equalsIgnoreCase("help")) {
                         p.sendMessage("§8§m---------------§8=§f§lTeamchat§8=§8§m---------------");
-                        p.sendMessage(TeamChat.prefix + "§ePlugin edited by Chaosmatter");
+                        p.sendMessage(TeamChat.prefix + "§dPlugin edited by Chaosmatter");
                         p.sendMessage(TeamChat.prefix + "§f§l/tc [Nachricht] §eSende eine Nachricht im Teamchat");
                         p.sendMessage(TeamChat.prefix + "§f§l/tc login/Logout §eLogge dich in den Teamchat ein/aus");
                         p.sendMessage(TeamChat.prefix + "§f§l/tc list §eZeige alle aktiven Teammitglieder und ihren");
+                        p.sendMessage(TeamChat.prefix + "§f§l/tc add/remove list §eZeigt/Versteckt deinen Server in der Teamliste");
                         p.sendMessage("§8§m---------------§8=§f§lTeamchat§8=§8§m---------------");
                     } else if (args[0].equalsIgnoreCase("list")) {
                         if (TeamChat.login.contains(p)) {
                             p.sendMessage("");
-                            p.sendMessage("§8----------=§f§lTeamchat§8=----------");
+                            p.sendMessage("§8§m---------------§8=§f§lTeamchat§8=§8§m---------------");
                             for (final ProxiedPlayer all : BungeeCord.getInstance().getPlayers()) {
                                 if (TeamChat.login.contains(all)) {
                                     if (!TeamChat.listed.contains(all)) {
@@ -134,7 +135,7 @@ public class tcCommand extends Command {
                                     }
                                 }
                             }
-                            p.sendMessage("§8----------=§f§lTeamchat§8=----------");
+                            p.sendMessage("§8§m---------------§8=§f§lTeamchat§8=§8§m---------------");
                         } else {
                             p.sendMessage(TeamChat.prefix + "§7Für diese Aktion musst du §aeingeloggt §7sein.");
                         }
@@ -142,10 +143,14 @@ public class tcCommand extends Command {
                         if (args[1].equalsIgnoreCase("list")) {
                             if (TeamChat.login.contains(p)) {
                                 if (!TeamChat.listed.contains(p)) {
-                                    TeamChat.listed.add(p);
-                                    p.sendMessage(TeamChat.prefix + " §7Dein §aaktueller Server §7ist nun im TeamChat §aversteckt§7!");
+                                    if(!p.hasPermission("tc.removelist")) {
+                                        p.sendMessage(TeamChat.noperms);
+                                    } else {
+                                        TeamChat.listed.add(p);
+                                        p.sendMessage(TeamChat.prefix + " §7Dein §aaktueller Server §7ist nun im TeamChat §aversteckt§7!");
+                                    }
                                 } else {
-                                    p.sendMessage(TeamChat.prefix + " §7Dein aktueller Server ist bereits nicht zu sehen!");
+                                    p.sendMessage(TeamChat.prefix + " §7Dein aktueller Server ist bereits versteckt!");
                                 }
                             } else {
                                 p.sendMessage(TeamChat.prefix + "§7Für diese Aktion musst du §aeingeloggt §7sein.");
@@ -155,8 +160,12 @@ public class tcCommand extends Command {
                         if (args[1].equalsIgnoreCase("list")) {
                             if (TeamChat.login.contains(p)) {
                                 if (TeamChat.listed.contains(p)) {
-                                    TeamChat.listed.remove(p);
-                                    p.sendMessage(TeamChat.prefix + " §7Dein §aaktueller Server §7ist nun wieder im TeamChat §azu sehen§7!");
+                                    if(!p.hasPermission("tc.addlist")) {
+                                        p.sendMessage(TeamChat.noperms);
+                                    } else {
+                                        TeamChat.listed.remove(p);
+                                        p.sendMessage(TeamChat.prefix + " §7Dein §aaktueller Server §7ist nun wieder im TeamChat §azu sehen§7!");
+                                    }
                                 } else {
                                     p.sendMessage(TeamChat.prefix + " §7Dein aktueller Server ist bereits zu sehen!");
                                 }
@@ -171,39 +180,40 @@ public class tcCommand extends Command {
                                     String nachricht = "";
                                     for (int i = 0; i < args.length; ++i) {
                                         nachricht = String.valueOf(nachricht) + " " + args[i];
+
                                     }
                                     if (p.hasPermission("rankcolor.4")) {
-                                        all.sendMessage(TeamChat.prefix + "§4" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§4" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.b")) {
-                                        all.sendMessage(TeamChat.prefix + "§b" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§b" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.c")) {
-                                        all.sendMessage(TeamChat.prefix + "§c" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§c" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.9")) {
-                                        all.sendMessage(TeamChat.prefix + "§9" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§9" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.1")) {
-                                        all.sendMessage(TeamChat.prefix + "§1" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§1" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.2")) {
-                                        all.sendMessage(TeamChat.prefix + "§2" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§2" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.3")) {
-                                        all.sendMessage(TeamChat.prefix + "§3" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§3" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.5")) {
-                                        all.sendMessage(TeamChat.prefix + "§5" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§5" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.6")) {
-                                        all.sendMessage(TeamChat.prefix + "§6" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§6" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.7")) {
-                                        all.sendMessage(TeamChat.prefix + "§7" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§7" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.8")) {
-                                        all.sendMessage(TeamChat.prefix + "§8" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§8" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.0")) {
-                                        all.sendMessage(TeamChat.prefix + "§0" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§0" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.a")) {
-                                        all.sendMessage(TeamChat.prefix + "§a" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§a" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.e")) {
-                                        all.sendMessage(TeamChat.prefix + "§e" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§e" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else if (p.hasPermission("rankcolor.d")) {
-                                        all.sendMessage(TeamChat.prefix + "§d" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§d" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     } else {
-                                        all.sendMessage(TeamChat.prefix + "§d" + p.getName() + " §8» §7" + nachricht);
+                                        all.sendMessage(TeamChat.prefix + "§f" + p.getName() + " §8» §7" + nachricht.replace("&", "§"));
                                     }
                                 } else {
                                     TeamChat.login.contains(all);
@@ -214,7 +224,13 @@ public class tcCommand extends Command {
                         p.sendMessage(TeamChat.prefix + "§7Für diese Aktion musst du §aeingeloggt §7sein.");
                     }
                 } else if (args.length == 0) {
-                    p.sendMessage(TeamChat.prefix + " §7Nutze: <§alogin §7| §clogout §7| §elist§7 | §bNachricht §7| §3add§7/§3remove list§7>");
+                    p.sendMessage("§8§m---------------§8=§f§lTeamchat§8=§8§m---------------");
+                    p.sendMessage(TeamChat.prefix + "§dPlugin edited by Chaosmatter");
+                    p.sendMessage(TeamChat.prefix + "§f§l/tc [Nachricht] §eSende eine Nachricht im Teamchat");
+                    p.sendMessage(TeamChat.prefix + "§f§l/tc login/Logout §eLogge dich in den Teamchat ein/aus");
+                    p.sendMessage(TeamChat.prefix + "§f§l/tc list §eZeige alle aktiven Teammitglieder und ihren");
+                    p.sendMessage(TeamChat.prefix + "§f§l/tc add/remove list §eZeigt/Versteckt deinen Server in der Teamliste");
+                    p.sendMessage("§8§m---------------§8=§f§lTeamchat§8=§8§m---------------");
                 }
             } else {
                 p.sendMessage(TeamChat.noperms);
